dab56fb4b2776c1f7dbee9099db37e1c60a12960
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2024-01-23 04:10:34 +0530

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+// src/main.rs
+
+use rust_adb::ADB; 
+
 fn main() {
-    println!("Hello, world!");
+    let adb = ADB::new("/path/to/adb"); 
+    match adb.refresh_device_list() {
+        Ok(devices) => {
+            println!("Connected devices:");
+            for device in devices {
+                println!("{}", device);
+            }
+        }
+        Err(err) => eprintln!("Error: {}", err),
+    }
+
+    
+    match adb.get_screenshot_png("device_id_or_serial") {
+        Ok(screenshot) => {
+           
+            println!("Screenshot received: {} bytes", screenshot.len());
+        }
+        Err(err) => eprintln!("Error getting screenshot: {}", err),
+    }
 }
